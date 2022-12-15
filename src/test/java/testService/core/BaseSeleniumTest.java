e3d9c46e89236425b16a3f72e478df9659ee2978
IrinaTt <turkina.ira@gmail.com>
2022-12-15 17:04:10 +0200

    uncomment hooks

--- src/test/java/testService/core/BaseSeleniumTest.java
+++ src/test/java/testService/core/BaseSeleniumTest.java
@@ -11,20 +11,20 @@ import java.time.Duration;
 abstract public class BaseSeleniumTest {
     protected WebDriver webDriver;
 
-//    @Before
-//    public void setUp() {
-//        WebDriverManager.chromedriver().setup();
-//        webDriver = new ChromeDriver();
-//        webDriver.manage().window().maximize();
-//        webDriver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
-//        webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-//
-//        BaseSeleniumPage.setDriver(webDriver);
-//    }
-//
-//    @After
-//    public void tearDown() {
-//        webDriver.close();
-//        webDriver.quit();
-//    }
+    @Before
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        webDriver = new ChromeDriver();
+        webDriver.manage().window().maximize();
+        webDriver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+        webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        BaseSeleniumPage.setDriver(webDriver);
+    }
+
+    @After
+    public void tearDown() {
+        webDriver.close();
+        webDriver.quit();
+    }
 }
